57c846ffc60a8c3bce75616a1528f9a0d3af6a2d
a9vn <ark9manager@gmail.com>
2026-01-02 15:35:31 +0700

    ad

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,24 @@
 import React, { useState } from "react";
 import Sidebar from "./components/UI/Sidebar";
 import SidebarToggle from "./components/UI/SidebarToggle";
-import SidebarOverlay from "./components/UI/SidebarOverlay";
+// import SidebarOverlay from "./components/UI/SidebarOverlay";
 import Player from "./components/Player/Player";
 import Display from "./pages/Display";
 
 const App = () => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
-  const toggleSidebar = () => {
-    setIsSidebarOpen(!isSidebarOpen);
-  };
-
-  const closeSidebar = () => {
-    setIsSidebarOpen(false);
-  };
+  const toggleSidebar = () => setIsSidebarOpen((prev) => !prev);
+  const closeSidebar = () => setIsSidebarOpen(false);
 
   return (
     <div className="h-screen bg-black">
       <SidebarToggle isOpen={isSidebarOpen} onToggle={toggleSidebar} />
-      <SidebarOverlay isOpen={isSidebarOpen} onClose={closeSidebar} />
       <div className="h-[90%] flex">
-        <Sidebar isOpen={isSidebarOpen} />
+        <Sidebar isOpen={isSidebarOpen} onClose={closeSidebar} />
         <Display />
       </div>
+
       <Player />
     </div>
   );
