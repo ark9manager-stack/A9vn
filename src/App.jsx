ef9f67f2a66c687f0641e2badeeac974eb11d404
kiyari005 <ark9manager@gmail.com>
2026-01-17 21:43:07 +0700

    scroll

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ const App = () => {
   return (
     <div className="h-screen bg-black">
       <Navbar isOpen={isSidebarOpen} onToggle={toggleSidebar} />
-      <div className="h-[90%] flex">
+      <div className="h-screen">
         <Sidebar isOpen={isSidebarOpen} onClose={closeSidebar} />
         <Display />
       </div>
