98f45431c0d2fb190d5b6e2af91cc50216022c5d
kiyari005 <ark9manager@gmail.com>
2026-01-20 21:02:18 +0700

    modal

--- src/components/Operator/OperatorModal.jsx
+++ src/components/Operator/OperatorModal.jsx
@@ -1,35 +1,36 @@
+// components/Operator/OperatorModal.jsx
 import React from "react";
 import OperatorHeader from "./modal/OperatorHeader";
 import OperatorProfile from "./modal/OperatorProfile";
 import OperatorStats from "./modal/OperatorStats";
-import OperatorSkills from "./modal/OperatorSkills";
 import OperatorTalents from "./modal/OperatorTalents";
+import OperatorSkills from "./modal/OperatorSkills";
 
 const OperatorModal = ({ operator, onClose }) => {
   if (!operator) return null;
 
   return (
     <div className="fixed inset-0 z-50 bg-black/80 flex items-center justify-center">
-      {/* Overlay */}
+      {/* overlay */}
       <div className="absolute inset-0" onClick={onClose} />
 
-      {/* Modal */}
+      {/* modal */}
       <div
-        className="relative w-[92%] max-w-6xl h-[90vh]
+        className="relative w-[95%] max-w-6xl h-[92vh]
         bg-[#121212] rounded-2xl shadow-2xl overflow-hidden flex
       "
       >
-        {/* Left */}
+        {/* LEFT */}
         <div className="w-[320px] bg-[#1a1a1a] border-r border-white/10">
           <OperatorHeader operator={operator} />
         </div>
 
-        {/* Right */}
+        {/* RIGHT */}
         <div className="flex-1 overflow-y-auto p-6 space-y-6">
           <OperatorProfile operator={operator} />
-          <OperatorStats operator={operator} />
-          <OperatorTalents operator={operator} />
-          <OperatorSkills operator={operator} />
+          <OperatorStats />
+          <OperatorTalents />
+          <OperatorSkills />
         </div>
       </div>
     </div>
