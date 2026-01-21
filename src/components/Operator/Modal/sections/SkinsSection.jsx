8f9ef31b572f08dee6cb51f527765930e817f85c
a9vn <ark9manager@gmail.com>
2026-01-22 01:25:42 +0700

    ed

--- src/components/Operator/Modal/sections/SkinsSection.jsx
+++ src/components/Operator/Modal/sections/SkinsSection.jsx
@@ -253,15 +253,6 @@ export default function SkinsSection({ operator, className = "" }) {
   return (
     <div className={`relative w-full h-full min-h-[520px] rounded-2xl overflow-hidden bg-black/20 ${className}`}>
       <div className="relative h-full w-full">
-        {/* Name (top-right) */}
-        <div className="absolute right-3 top-3 z-20 text-right">
-          <div className="text-2xl font-extrabold text-white leading-tight drop-shadow">
-            {titleText}
-          </div>
-          <div className="text-xs font-semibold text-white/85 drop-shadow">
-            {charId}
-          </div>
-        </div>
 
         {/* Art */}
         <div className="absolute inset-0 flex items-center justify-center">
