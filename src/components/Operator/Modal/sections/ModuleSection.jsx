4a0d80201b9dd51c892f76729cbc75a352fcbab0
kiyari005 <ark9manager@gmail.com>
2026-02-05 17:26:47 +0700

    Update ModuleSection.jsx

--- src/components/Operator/Modal/sections/ModuleSection.jsx
+++ src/components/Operator/Modal/sections/ModuleSection.jsx
@@ -1,4 +1,11 @@
 import React from "react";
 
-const ModuleSection = {};
+const ModuleSection = () => {
+  return (
+    <div className="header">
+      <span className="title">module</span>
+    </div>
+  );
+};
+
 export default ModuleSection;
