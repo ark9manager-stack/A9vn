2647a92652208aa4629d78a4636cf0f0230204d9
a9vn <ark9manager@gmail.com>
2026-02-08 18:34:29 +0700

    ed

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,41 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { Navigate, Route, Routes, useLocation } from "react-router-dom";
+
 import Sidebar from "./components/UI/Sidebar";
 import Navbar from "./components/UI/Navbar";
-// import SidebarOverlay from "./components/UI/SidebarOverlay";
 
-import Display from "./pages/Display";
+import Home from "./pages/Home";
+import Operator from "./pages/Operator";
+import Music from "./pages/Music";
 
 const App = () => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const location = useLocation();
+  useEffect(() => {
+    setIsSidebarOpen(false);
+  }, [location.pathname]);
 
   const toggleSidebar = () => setIsSidebarOpen((prev) => !prev);
   const closeSidebar = () => setIsSidebarOpen(false);
 
   return (
-    <div className="flex  bg-black">
+    <div className="flex bg-black h-screen w-screen overflow-hidden">
       <Navbar isOpen={isSidebarOpen} onToggle={toggleSidebar} />
       <Sidebar isOpen={isSidebarOpen} onClose={closeSidebar} />
-      <Display />
+
+      <div className="flex-1 h-full">
+        <Routes>
+          <Route path="/" element={<Navigate to="/Home" replace />} />
+          <Route path="/Home" element={<Home />} />
+          <Route path="/Operator" element={<Operator />} />
+          <Route path="/Music" element={<Music />} />
+          <Route path="/Operator=:operatorId" element={<Operator />} />
+          <Route path="/home" element={<Navigate to="/Home" replace />} />
+          <Route path="/operator" element={<Navigate to="/Operator" replace />} />
+          <Route path="/music" element={<Navigate to="/Music" replace />} />
+          <Route path="*" element={<Navigate to="/Home" replace />} />
+        </Routes>
+      </div>
     </div>
   );
 };
