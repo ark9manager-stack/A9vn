5f30296f79e19530151d816bf4c9617f04333852
kiyari005 <ark9manager@gmail.com>
2026-02-24 13:57:09 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -20,23 +20,17 @@ const App = () => {
     <div className="flex bg-black">
       <Navbar isOpen={isSidebarOpen} onToggle={toggleSidebar} />
       <Sidebar isOpen={isSidebarOpen} onClose={closeSidebar} />
-
-      <div className="flex-1 h-screen">
-        <Routes>
-          <Route path="/" element={<Navigate to="/Home" replace />} />
-          <Route path="/Home" element={<Display />} />
-          <Route path="/Operator" element={<Display />} />
-          <Route path="/Music" element={<Display />} />
-          <Route path="/home" element={<Navigate to="/Home" replace />} />
-          <Route
-            path="/operator"
-            element={<Navigate to="/Operator" replace />}
-          />
-          <Route path="/music" element={<Navigate to="/Music" replace />} />
-          <Route path="/:slug" element={<Display />} />
-          <Route path="*" element={<Navigate to="/Home" replace />} />
-        </Routes>
-      </div>
+      <Routes>
+        <Route path="/" element={<Navigate to="/Home" replace />} />
+        <Route path="/Home" element={<Display />} />
+        <Route path="/Operator" element={<Display />} />
+        <Route path="/Music" element={<Display />} />
+        <Route path="/home" element={<Navigate to="/Home" replace />} />
+        <Route path="/operator" element={<Navigate to="/Operator" replace />} />
+        <Route path="/music" element={<Navigate to="/Music" replace />} />
+        <Route path="/:slug" element={<Display />} />
+        <Route path="*" element={<Navigate to="/Home" replace />} />
+      </Routes>
     </div>
   );
 };
