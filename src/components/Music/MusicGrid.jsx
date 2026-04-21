0d771102d408ff8a6f0922ea5afd017d12d86789
kiyari005 <ark9manager@gmail.com>
2026-04-21 17:16:57 +0700

    Update MusicGrid.jsx

--- src/components/Music/MusicGrid.jsx
+++ src/components/Music/MusicGrid.jsx
@@ -4,19 +4,19 @@ import MusicCard from "./MusicCard";
 
 const MusicGrid = ({ songs, startIndex, onSelectMusic }) => {
   return (
-    <AnimatedContent className="mb-8">
+    <div className="mb-8">
       <div className="music-grid mb-8">
         {songs.map((item, index) => (
-          <AnimatedContent key={item.id} className="animate-in">
+          <div key={item.id} className="animate-in">
             <MusicCard
               item={item}
               index={startIndex + index}
               onClick={() => onSelectMusic(item)}
             />
-          </AnimatedContent>
+          </div>
         ))}
       </div>
-    </AnimatedContent>
+    </div>
   );
 };
 
